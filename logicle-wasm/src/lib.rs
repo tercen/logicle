4aad3ba923a9dc4b1193f3c14446dc56270f09a7
Alexandre Maurel <alexandre.maurel@gmail.com>
2024-12-05 15:51:20 +0100

    remove useless

--- logicle-wasm/src/lib.rs
+++ logicle-wasm/src/lib.rs
@@ -29,27 +29,3 @@ impl LogicleW {
     }
 }
 
-#[wasm_bindgen]
-extern "C" {
-    pub fn alert(s: &str);
-}
-
-#[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(&format!("Hello, {}!", name));
-}
-
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
